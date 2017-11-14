a33c68e41c7ffc4047748bcf04ae4b77d2a96859
DmncG <drbgarcia389@yahoo.com>
2017-11-14 17:54:34 -0500

    animation for projects

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
 
-
 ReactDOM.render(
   <Routes/>,
   document.getElementById('app')
-);
-
-
-
-
-
-
-
-
+)
