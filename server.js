64a6da027b96e1cf54e6ff4a6ee33a33d6b95e87
DmncG <drbgarcia389@yahoo.com>
2017-11-14 20:58:33 -0500

    express server for deployment

---
+++ server.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 8080
+const app = express()
+module.exports = app
+
+/**
+ * In your development environment, you can keep all of your
+ * app's secret API keys in a file called `secrets.js`, in your project
+ * root. This file is included in the .gitignore - it will NOT be tracked
+ * or show up on Github. On your production server, you can add these
+ * keys as environment variables, so that they can still be read by the
+ * Node process on process.env
+ */
+
+// passport registration
+
+const createApp = () => {
+  // body parsing middleware
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+}
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+
+createApp()
+startListening()
