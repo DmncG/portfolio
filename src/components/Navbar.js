0834c476cd1082253d93ce10de3a9eb203843845
DmncG <drbgarcia389@yahoo.com>
2017-11-17 18:45:18 -0500

    feat floating button changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,34 @@ import {Home, Works, About} from '../components'
 import Menu from 'material-ui-icons/Menu'
 import AppBar from 'material-ui/AppBar'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ArrowBack from 'material-ui-icons/ArrowBack'
 
-const Navbar = props => {
-  return (
-    <div>
-      <FloatingActionButton className='navbar-floating-btn'>
-        <Menu/>
-      </FloatingActionButton>
-      <div className="navbar-headers">
-        <NavLink className="navbar-home" to="/home">  Home  </NavLink>
-        <NavLink to="/works">  Works  </NavLink>
-        <NavLink to="/about">  About  </NavLink>
-        <p className="navbar-test"> Bounce </p>
+class Navbar extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      hovering: false,
+      clicked: false
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.clicked
+          ? <FloatingActionButton className='navbar-floating-btn' onClick={() => this.setState({clicked: false})}> <ArrowBack/> </FloatingActionButton>
+          : <FloatingActionButton className='navbar-floating-btn' onClick={() => this.setState({clicked: true})}> <Menu/> </FloatingActionButton>
+        }
+        <div className="navbar-headers">
+          <NavLink className="navbar-home" to="/home">  Home  </NavLink>
+          <NavLink to="/works">  Works  </NavLink>
+          <NavLink to="/about">  About  </NavLink>
+          <div className="navbar-test"></div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Navbar
