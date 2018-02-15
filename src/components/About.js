2c794977ea202ec5fe0e9d2dd7d016f98bacaa14
DmncG <drbgarcia389@yahoo.com>
2018-02-15 00:12:56 -0500

    fix styling media queries

--- src/components/About.js
+++ src/components/About.js
@@ -18,69 +18,69 @@ class About extends Component {
 
   render () {
     return (
-        <div>
-          <Navbar/>
-          <Particles className='particles-bg' params={{
-            particles: {
-              line_linked: {
-                enable: true
-              },
-              number: {
-                value: 5
-              },
-              color: {
-                value: '#000000'
-              },
-              size: {
-                value: 7.5
-              }
+      <div>
+        <Navbar/>
+        <Particles className='particles-bg' params={{
+          particles: {
+            line_linked: {
+              enable: true
             },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: false,
-                  mode: 'repulse'
-                }
+            number: {
+              value: 5
+            },
+            color: {
+              value: '#000000'
+            },
+            size: {
+              value: 7.5
+            }
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: false,
+                mode: 'repulse'
               }
             }
-          }} style={{'minHeight': '100%', 'height': 'auto', width: '100%', position: 'absolute', zIndex: -1}} canvasClassName="particle-canvas"/>
-          <Grid>
-            <Row>
-              <Col xs={12} md={7} lg={7} className="about-col-profile">
-                <img className="about-profile" src="../../assets/images/about_profile.jpg"/>
-              </Col>
-              <Col xs={12} md={5} lg={5} className="about-col-content">
-                <div id="about-content">
-                  <p className="about-header">
-                About Me
-                  </p>
-                  <p className="about-p1">
-                    <strong>
-                        I am a recent graduate of Fullstack Academy's
-                        Software Engineering Immersive where I learned
-                        how to build various applications.
-                    </strong>
-                  </p>
-                  <p className="about-p1">
-                    <strong>
-                        I worked in a non-profit organization for the past six years
-                        until a few friends of mine introduced me to
-                        software development--a world where I could express my
-                        imagination and creativity in interesting and meaningful ways.
-                    </strong>
-                  </p>
-                  <p id="about-p2">
-                    <strong>
-                        Asides from writing code, I also err... write! I love conjuring stories: 
-                        preferably something that involves mazes, gods, monsters and
-                        everything in between.
-                    </strong>
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </Grid>
-        </div>
+          }
+        }} style={{'minHeight': '100%', 'height': 'auto', width: '100%', position: 'absolute', zIndex: -1}} canvasClassName="particle-canvas"/>
+        <Grid>
+          <Row>
+            <Col xs={12} md={7} lg={7} className="about-col-profile">
+              <img className="about-profile" src="../../assets/images/about_profile.jpg"/>
+            </Col>
+            <Col xs={12} md={5} lg={5} className="about-col-content">
+              <div id="about-content">
+                <p className="about-header">
+              About Me
+                </p>
+                <p className="about-p1">
+                  <strong>
+                      I am a recent graduate of Fullstack Academy's
+                      Software Engineering Immersive where I learned
+                      how to build various applications.
+                  </strong>
+                </p>
+                <p className="about-p1">
+                  <strong>
+                      I worked in a non-profit organization for the past six years
+                      until a few friends of mine introduced me to
+                      software development--a world where I could express my
+                      imagination and creativity in interesting and meaningful ways.
+                  </strong>
+                </p>
+                <p id="about-p2">
+                  <strong>
+                      Asides from writing code, I also err... write! I love conjuring stories: 
+                      preferably something that involves mazes, gods, monsters and
+                      everything in between.
+                  </strong>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     )
   }
 }
--- src/components/Works.js
+++ src/components/Works.js
@@ -27,104 +27,102 @@ class Works extends Component {
     //  scroll animation for projects
 
     return (
-      
-        <div>
-          <Navbar/>
-          <Particles className='particles-bg' params={{
-            particles: {
-              line_linked: {
-                enable: false
-              },
-              number: {
-                value: 5
-              },
-              color: {
-                value: '#000000'
-              },
-              size: {
-                value: 10
-              }
+      <div>
+        <Navbar/>
+        <Particles className='particles-bg' params={{
+          particles: {
+            line_linked: {
+              enable: false
             },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: false,
-                  mode: 'repulse'
-                }
+            number: {
+              value: 5
+            },
+            color: {
+              value: '#000000'
+            },
+            size: {
+              value: 10
+            }
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: false,
+                mode: 'repulse'
               }
             }
-          }} style={{'minHeight': '100%', width: '100%', position: 'fixed', zIndex: -1}}/>
-          <div className="works-content">
-            {/* srcs.length &&
-                srcs.map((source, i) => {
-                  return (
-                    <iframe key={i} id="ytplayer" type="text/html" width="320" height="180"
-                      src={`https://www.youtube.com/embed/${source}?autoplay=0`}
-                      frameBorder="0"></iframe>
-                  )
-                })
-            */}
-            <Grid>
-              <Row>
-                <Col xs={12} md={1} lg={1} className="works-col-header">
-                  <Waypoint onEnter={function () { $('section').addClass('active') }}/>
-                  <section id="works-header-anim">
-                    <p className="works-header">
-                      Works
+          }
+        }} style={{'minHeight': '100%', width: '100%', position: 'fixed', zIndex: -1}}/>
+        <div className="works-content">
+          {/* srcs.length &&
+              srcs.map((source, i) => {
+                return (
+                  <iframe key={i} id="ytplayer" type="text/html" width="320" height="180"
+                    src={`https://www.youtube.com/embed/${source}?autoplay=0`}
+                    frameBorder="0"></iframe>
+                )
+              })
+          */}
+          <Grid>
+            <Row>
+              <Col xs={12} md={1} lg={1} className="works-col-header">
+                <Waypoint onEnter={function () { $('section').addClass('active') }}/>
+                <section id="works-header-anim">
+                  <p className="works-header">
+                    Works
+                  </p>
+                </section>
+                <div className="works-header-dash">
+                </div>
+              </Col>
+              <Col xs={12} md={10} lg={10} className="works-col-content">
+                <div className="works-saffire">
+                  <div className="works-saffire-box">
+                    <a href="https://github.com/CapstoneFabolous/Travel-App" target="_blank">
+                      <img className="works-saffire-screenshot" src="/assets/images/saffire_Screenshot.png"/>
+                    </a>
+                  </div>
+                  <div className="works-saffire-header">
+                    <p>
+                  Saffire
                     </p>
-                  </section>
-                  <div className="works-header-dash">
                   </div>
-                </Col>
-                <Col xs={12} md={10} lg={10} className="works-col-content">
-                  <div className="works-saffire">
-                    <div className="works-saffire-box">
-                      <a href="https://github.com/CapstoneFabolous/Travel-App" target="_blank">
-                        <img className="works-saffire-screenshot" src="/assets/images/saffire_Screenshot.png"/>
-                      </a>
-                    </div>
-                    <div className="works-saffire-header">
-                      <p>
-                    Saffire
-                      </p>
-                    </div>
+                </div>
+                <div className="works-meeseeks">
+                  <div className="works-meeseeks-box">
+                    <a href="https://github.com/DmncG/stackathon_phaser" target="_blank">
+                      <img className="works-meeseeks-gif" src="/assets/images/meeseeks_Screenshot.png"/>
+                    </a>
                   </div>
-                  <div className="works-meeseeks">
-                    <div className="works-meeseeks-box">
-                      <a href="https://github.com/DmncG/stackathon_phaser" target="_blank">
-                        <img className="works-meeseeks-gif" src="/assets/images/meeseeks_Screenshot.png"/>
-                      </a>
-                    </div>
-                    <div className="works-meeseeks-header">
-                      <p>
-                        Meeseeks <br/>&Destroy
-                      </p>
-                    </div>
+                  <div className="works-meeseeks-header">
+                    <p>
+                      Meeseeks <br/>&Destroy
+                    </p>
                   </div>
-                  <div className="works-threejs">
-                    <div className="works-threejs-box">
-                      <p className="works-threejs-logo">
-                      three.js
-                      </p>
-                    </div>
-                    <div className="works-threejs-header">
-                      <p>
-                        Three.js Talk
-                      </p>
-                    </div>
+                </div>
+                <div className="works-threejs">
+                  <div className="works-threejs-box">
+                    <p className="works-threejs-logo">
+                      <a href="https://www.youtube.com/watch?v=nRGFzdYnl_c" target="_blank" style={{color: 'inherit'}}>three.js</a>
+                    </p>
                   </div>
-                  <br/>
-                  <br/>
-                </Col>
-                <Col xs={12} md={1} lg={1} className="works-col-header">
-                  <div>
+                  <div className="works-threejs-header">
+                    <p>
+                      Three.js Talk
+                    </p>
                   </div>
-                </Col>
-              </Row>
-            </Grid>
-          </div>
+                </div>
+                <br/>
+                <br/>
+              </Col>
+              <Col xs={12} md={1} lg={1} className="works-col-header">
+                <div>
+                </div>
+              </Col>
+            </Row>
+          </Grid>
         </div>
-      
+      </div>
     )
   }
 }
